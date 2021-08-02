2d730178932eafb50f933b35118598462027283f
Michael Klemm <michael@dontknow.de>
2021-08-02 13:52:38 +0200

    Add simple test case for task with if(0) clause

---
+++ tests/test_tasks_if0.c
@@ -0,0 +1,35 @@
+//===-- test_tasks.c - Test task creation and execution  ----------*- C -*-===//
+//
+// Part of the LOMP project, under the Apache License v2.0 with LLVM Exceptions.
+// See https://llvm.org/LICENSE.txt for license information.
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+//
+//===----------------------------------------------------------------------===//
+
+#include <stdio.h>
+#include <stdlib.h>
+
+#define NTASKS 33
+
+int main(void) {
+  int count = 0;
+
+#pragma omp parallel shared(count)
+#pragma omp master
+  {
+    for (int i = 0; i < NTASKS; i++) {
+#pragma omp task shared(count) if(0)
+      {
+#pragma omp atomic
+        count++;
+      }
+    }
+  }
+
+  int failed = (count != NTASKS);
+  printf("Got %d tasks, should be %d\n"
+         "***%s***\n",
+         count, NTASKS, failed ? "FAILED" : "PASSED");
+  return failed ? EXIT_FAILURE : EXIT_SUCCESS;
+  ;
+}
